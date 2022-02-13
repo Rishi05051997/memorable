81b059c9cca74f6ce802ff2b0f3acbb7a38cf6b9
rishi05051997 <vrushabhdhatral10@gmail.com>
2022-02-13 13:42:18 +0530

    Fixed : Add modal.js file,  style fixed

---
+++ script/modal.js
@@ -0,0 +1,33 @@
+///// Modal Work 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.modal-close');
+
+
+const closedModal = () => {
+    modal.classList.add('blck_');
+    overlay.classList.add('blck_');
+}
+
+const openedModal = () => {
+    modal.classList.remove('blck_');
+    overlay.classList.remove('blck_');
+}
+
+const closedModalByEscapeKey = (e) => {
+    if (e.key === "Escape" && !modal.classList.contains('blck_')) {
+        closedModal();
+    }
+}
+var hello = "hello";
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openedModal);
+}
+
+closeModal.addEventListener('click', closedModal);
+
+overlay.addEventListener('click', closedModal);
+
+document.addEventListener('keydown', closedModalByEscapeKey);
